48d0ab69bc2043ef4e9255332960c4e6c5701f71
JulioH-dot <juliohenrique1909@gmail.com>
2025-03-04 19:40:22 -0300

    feat: first commit

---
+++ src/test/java/com/henrique/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
